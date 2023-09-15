a988b91e218f9a914a499fb28e185ce33fb1a7fe
Heitor Novais <hnovais95@gmail.com>
2023-09-15 19:59:50 -0300

    show a friendly message in index page

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/app', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
 
